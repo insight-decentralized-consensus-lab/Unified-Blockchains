5eecc62420c4249a6e371508d92ea92ae660b3a2
Dragos Velicanu <velicanu@mit.edu>
2018-09-18 12:05:08 -0400

    update unified json maker

--- block-json-to-unified.py
+++ block-json-to-unified.py
@@ -1,16 +1,59 @@
 import json
 import sys, os
+import copy
 
-if(len(sys.argv)>1):
+if(len(sys.argv)>2):
     infile = sys.argv[1]
+    outfile = sys.argv[2]
 else:
     print("run via:")
-    print("python3 "+sys.argv[0]+" <input-file>")
+    print("python3 "+sys.argv[0]+" <input-file> <output-file>")
     sys.exit(1)
-    
+
 with open(infile) as f:
     data = json.load(f)
+    
+try: # needed for genesis block which has no parent
+    parent = data["previousblockhash"]
+except:
+    parent = 0
 
-print(data)
+unifiedblock = {}
+unifiedblock["hash"] = data["hash"]
+unifiedblock["parent"] = parent
+unifiedblock["child"] = data["nextblockhash"]
+unifiedblock["num"] = data["height"]
+tx_list = []
+for i in data["tx"] :
+    tx = {}
+    tx["hash"] = i["hash"]
+    try:
+        _from = i["vin"][0]["txid"]
+    except:
+        _from = "miner"
+    tx["from"] = _from
+    # tx["coin"] = "btc"
+    # tx["blocknum"] = unifiedblock["num"]
+    # tx["blockhash"] = unifiedblock["hash"]
+    # tx["time"] = unifiedblock["time"]
+    
+    for j in i["vout"]:
+        tx["value"] = j["value"]
+        tx["to"] = j["scriptPubKey"]["addresses"][0]
+        tx_list.append(copy.deepcopy(tx))
 
-                            
+unifiedblock["tx"] = tx_list
+unifiedblock["time"] = data["time"]
+unifiedblock["coin"] = "btc"
+
+# unifiedtx = []
+# for i in data["tx"] :
+    
+# unifiedaddress = []
+
+
+# print(unifiedblock)
+# print(unifiedtx)
+with open(outfile, 'w') as outfile:
+    json.dump(unifiedblock, outfile, indent=4, sort_keys=True)
+
