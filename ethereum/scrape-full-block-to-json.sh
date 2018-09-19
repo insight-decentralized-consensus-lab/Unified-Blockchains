e051819ddf11e2626b0d1da7b2116402e11f30b4
Ubuntu <ubuntu@ip-10-0-0-9.ec2.internal>
2018-09-19 17:33:46 +0000

    parallel block scraper

--- ethereum/scrape-full-block-to-json.sh
+++ ethereum/scrape-full-block-to-json.sh
@@ -1,8 +1,24 @@
-for i in `seq 0 6350455`
+block_range() {
+    for i in `seq $1 $2`
+    do
+	if [ $(( $i % 1000 )) -eq 0 ]
+	then
+	    echo $i
+	fi
+	hex=$( printf "%X" $i )
+	outfile=$( printf "/home/ubuntu/ethereum-json/%07d.json \n" $i )
+	curl -s --data '{"method":"eth_getBlockByNumber","params":["0x'${hex}'",true],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:8545 -o $outfile
+    done
+}
+
+step=$((1000000 / $1))
+echo $step
+
+for i in `seq 0 $(($1-1))`
 do
-    hex=$( printf "%X" $i )
-    # echo $hex
-    outfile=$( printf "/home/ubuntu/ethereum-json/%06d.json \n" $i )
-    curl --data '{"method":"eth_getBlockByNumber","params":["0x'${hex}'",true],"id":1,"jsonrpc":"2.0"}' -H "Content-Type: application/json" -X POST localhost:8545 -o $outfile
+    start=$(($i * $step))
+    end=$(($i * $step + $step))
+    block_range $start $end &
 done
 
+wait
