c8c10971adeaf006402d7154361c1071d35932fd
Dragos Velicanu <velicanu@mit.edu>
2018-10-09 19:42:40 +0000

    more cleanup

--- bitcoin/archive-to-s3.sh
+++
@@ -1,6 +0,0 @@
-for i in `comm -12 <(cd ~/bitcoin-json; find . -name "*.json" | sort) <(cd ~/unified-bitcoin-json; find . -name "*.json" | sort)`
-do
-    aws s3 cp ~/bitcoin-json/$i s3://bitcoin-json-raw/
-    rm ~/bitcoin-json/$i
-done
-
