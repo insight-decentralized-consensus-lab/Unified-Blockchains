fc79c516fad05dac71840037f1601e979279cea6
Dragos Velicanu <velicanu@mit.edu>
2018-10-09 19:26:58 +0000

    adding first time setup scripts

--- bitcoin/initial-config.sh
+++ bitcoin/initial-config.sh
@@ -5,11 +5,16 @@ bitcoinjson=${name:-$default}
 
 default=$HOME/unified-bitcoin-json
 echo "Set unified-bitcoin-json dir, default: [$default]"
-read -p '' unifiedbitcoinjson
-unifiedbitcoinjson=${name:-$default}
+read -p '' unifiedjson
+unifiedjson=${name:-$default}
 
+default="bitcoin-json-raw"
+echo "Set s3 bucket, default: [$default]"
+read -p '' s3bucket
+s3bucket=${name:-$default}
 
-# mkdir 
-# mkdir $HOME/unified-bitcoin-json
+echo bitcoinjson: $bitcoinjson > bitcoin.config
+echo unifiedjson: $unifiedjson >> bitcoin.config
+echo s3bucket: $s3bucket >> bitcoin.config
 
-
+echo "Done. Settings saved in bitcoin.config"
--- bitcoin/initial-setup.sh
+++ bitcoin/initial-setup.sh
@@ -1,2 +1,40 @@
-mkdir $HOME/bitcoin-json
-mkdir $HOME/unified-bitcoin-json
+if [ ! -f bitcoin.config ]
+then
+    echo "No bitcoin.config file found, run initial-config.sh first"
+    exit 1
+fi
+bitcoinjson=$(grep bitcoinjson: bitcoin.config | awk '{print $2}')
+unifiedjson=$(grep unifiedjson: bitcoin.config | awk '{print $2}')
+
+if [ -z $bitcoinjson ]
+then
+    echo "Error: bitcoin.config exists but bitcoinjson missing, run initial-config.sh again"
+    exit 1
+fi
+if [ -z $unifiedjson ]
+then
+    echo "Error: bitcoin.config exists but unifiedjson missing, run initial-config.sh again"
+    exit 1
+fi
+mkdir -p $bitcoinjson
+mkdir -p $unifiedjson
+
+s3bucket=$(grep s3bucket: bitcoin.config | awk '{print $2}')
+if [ -z $s3bucket ]
+then
+    echo "Note: s3 not set up, s3 archiving will be turned off. Make sure you have enough free space (more than 1TB is required)"
+    echo "s3status: OFF" > setup.config
+else
+    s3status=$(aws s3 ls s3:// | grep $s3bucket | awk '{print $3}')
+    if [ -z $s3status ] 
+    then
+	echo "Error: s3 is set up but misconfigured, double check bucket configuration"
+	exit 1
+    else
+	echo "s3status: ON" > setup.config
+    fi
+fi
+
+echo "success: True" >> setup.config
+echo "Setup successful."
+
