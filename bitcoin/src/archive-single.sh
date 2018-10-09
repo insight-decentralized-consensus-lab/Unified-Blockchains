18e87dc778c1abda52dbee39765cd22a4e60ed8b
Dragos Velicanu <velicanu@mit.edu>
2018-10-09 21:15:02 +0000

    removed archive hardcode

--- bitcoin/src/archive-single.sh
+++ bitcoin/src/archive-single.sh
@@ -1,11 +1,14 @@
-if [ ! -z $(grep "s3status: ON" ../setup.config | awk '{print $2}') ]
-then
-    aws s3 cp ~/bitcoin-json/$1 s3://bitcoin-json-raw/
-fi
 bitcoinjson=$(grep bitcoinjson: ../bitcoin.config | awk '{print $2}')
 unifiedjson=$(grep unifiedjson: ../bitcoin.config | awk '{print $2}')
 unifiedhost=$(grep unifiedhost: ../bitcoin.config | awk '{print $2}')
 unifiedpath=$(grep unifiedpath: ../bitcoin.config | awk '{print $2}')
+
+if [ ! -z $(grep "s3status: ON" ../setup.config | awk '{print $2}') ]
+then
+    s3bucket=$(grep s3bucket: ../bitcoin.config | awk '{print $2}')
+    aws s3 cp $bitcoinjson/$1 s3://$s3bucket/
+fi
+
 
 scp $unifiedjson/$1 $unifiedhost:$unifiedpath/$1
 rm $bitcoinjson/$1
