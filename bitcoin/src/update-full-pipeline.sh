38417d918125e42e71b315b252ce433afa089c1e
Dragos Velicanu <velicanu@mit.edu>
2018-10-09 21:10:03 +0000

    update filename info

--- bitcoin/src/update-full-pipeline.sh
+++ bitcoin/src/update-full-pipeline.sh
@@ -6,10 +6,11 @@ unifiedjson=$(grep unifiedjson: ../bitcoin.config | awk '{print $2}')
 
 for i in `seq $1 $current_block`
 do
+    filename=$(printf "%06d.json" i)
     if [ ! -f $unifiedjson/$i.json ]
     then
-	bitcoin-cli getblockhash $i | xargs -I % bitcoin-cli getblock % 2 > $bitcoinjson/$i.json
-	python3 single-raw-to-unified.py /home/ubuntu/bitcoin-json/$i.json $unifiedjson/$i.json
-	./archive-single.sh $i.json &
+	bitcoin-cli getblockhash $i | xargs -I % bitcoin-cli getblock % 2 > $bitcoinjson/$filename
+	python3 single-raw-to-unified.py /home/ubuntu/bitcoin-json/$filename $unifiedjson/$filename
+	./archive-single.sh $filename &
     fi
 done
