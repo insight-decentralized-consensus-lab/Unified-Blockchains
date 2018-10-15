25adf5958bfa03ae0edd5fb8efab32c4e20bba61
Ubuntu <ubuntu@ip-10-0-0-9.ec2.internal>
2018-10-15 20:06:10 +0000

    updating updater'

--- ethereum/keep-updated.sh
+++ ethereum/keep-updated.sh
@@ -1,4 +1,17 @@
-last_block=6396847
+#!/usr/bin/env bash
+if [ -z $1 ]
+then
+    echo "Error: need to input the latest block the pipeline has already processed"
+    echo "usage: ./keep-updated.sh 545054"
+    exit 1
+else
+    last_block=$1
+fi
+if [ ! $(grep "success: True" ../setup.config | awk '{print $2}') ]
+then
+    echo "Error: pipeline needs to be setup first. Run ./initial-config.sh followed by ./initial-setup.sh from bitcoin directory"
+    exit 1
+fi
 
 while true
 do
